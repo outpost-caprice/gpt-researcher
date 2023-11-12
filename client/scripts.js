c1426b6e11a98ef6fef004ac0f035fcb53431609
outpost-caprice <itani@outpost-corp.com>
2023-11-12 15:23:13 +0900

    modified:   client/index.html     Fixing HTML form fields 	modified:   client/scripts.js     Correction to match ID 	modified:   client/styles.css

--- client/scripts.js
+++ client/scripts.js
@@ -37,7 +37,7 @@ const GPTResearcher = (() => {
       };
   
       socket.onopen = (event) => {
-        document.querySelector('textarea[name="task"]').value;
+        const task = document.querySelector('textarea[name="task"]').value;
         const report_type = document.querySelector('select[name="report_type"]').value;
         const agent = document.querySelector('input[name="agent"]:checked').value;
   
